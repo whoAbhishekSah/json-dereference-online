6d4ac3544db4838f0ce2d386e497c90a7bf1b33a
Abhishek Sah <abhishek.sah@go-jek.com>
2020-09-14 19:15:06 +0530

    [Abhishek] Deref schema

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Input, Button, Row, Col } from "antd";
+import deref from "json-schema-deref";
+import "./App.css";
+const { TextArea } = Input;
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const InputForm = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log("Success:");
+    let inputSchema = JSON.parse(values.schema.replace(/\n/g, ""));
+    deref(inputSchema, function (err, fullSchema) {
+      console.dir(fullSchema);
+      form.setFieldsValue({output: JSON.stringify(fullSchema)})
+
+    });
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      form={form}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Schema"
+        name="schema"
+        rules={[
+          {
+            required: true,
+            message: "Please input your schema!",
+          },
+        ]}
+      >
+        <TextArea
+          rows={30}
+          style={{ width: "40%", overflowY: "scroll", resize: "none" }}
+        />
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+      <Form.Item
+        label="Output schema"
+        name="output"
+      >
+        <TextArea rows={30} style={{ width: "40%", overflowY: "scroll", resize: "none" }} />
+      </Form.Item>
+    </Form>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputForm />
     </div>
   );
 }
